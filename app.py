dfbda30b1f425b1cd4dd96149f01ac044e9c409f
Slack Crow <slackcrow@icloud.com>
2019-02-13 19:40:05 -0500

    small

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/getListings')
+def returnListings():
+    dictToReturn = dict()
+    listToReturn = ['Test1','Active','Test Item']
+    dictToReturn['test1'] = listToReturn
+    return json.dumps(dictToReturn)
 
 if __name__ == '__main__':
     app.run(debug = True)
