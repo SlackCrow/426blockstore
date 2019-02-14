d3804d57d25bb77944ae43bf68329fb69524ad9d
Jason Pettrone <jasonpettrone@gmail.com>
2019-02-13 20:36:52 -0500

    Added basic login page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import json
 app = Flask(__name__)
 
@@ -15,6 +15,17 @@ def returnListings():
     dictToReturn['test3'] = ['Test 3','Sold','Test Item 3']
 
     return json.dumps(dictToReturn)
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template('index.html')
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug = True)
